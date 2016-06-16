fbfcba8760219c223b48e2a06998b5f586d00761
orsereiparinha <orsereiparinha@gmail.com>
2016-06-16 18:00:31 +0700

    fix mailer again

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,10 +2,10 @@ class Contact < MailForm::Base
 	attribute :name,		:validate 	=> true
 	attribute :email,		:validate 	=> /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 	attribute :message,		:validate 	=> true
-	attribute :subject,		:validate	=>	true
 
 	def headers 
 	 {
+		:subject => "Contact Form",
 		:to => "ospfolio@gmail.com",
 		:from => %("#{name}" <#{email}>)
 	 }
