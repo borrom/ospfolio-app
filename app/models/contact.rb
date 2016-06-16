f374e4164d20d07bdff0df268b5135156dd10933
orsereiparinha <orsereiparinha@gmail.com>
2016-06-16 22:33:29 +0700

    add subject contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,11 +2,12 @@ class Contact < MailForm::Base
 	attribute :name,		:validate 	=> true
 	attribute :email,		:validate 	=> /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 	attribute :message,		:validate 	=> true
+	attribute :subject,		:validate	=> true
 	attribute :nickname,	:captcha	=> true
 
 	def headers 
 	 {
-		:subject => "Contact Form",
+		:subject => %("#{subject}"),
 		:to => "ospfolio@gmail.com",
 		:from => %("#{name}" <#{email}>)
 	 }
