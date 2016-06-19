cbcb5954a1f80151829bb046e40ef7a3e843305b
orsereiparinha <orsereiparinha@gmail.com>
2016-06-19 10:01:15 +0700

    fix asset pipeline

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "2.2.3"
 gem 'rails', '4.2.6'
 gem 'bootstrap-sass'
 gem 'font-awesome-rails'
+gem 'ionicons-rails'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :admins
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :projects
   get 'contacts' => 'contact#index'
   post 'contacts' => 'contact#create'
